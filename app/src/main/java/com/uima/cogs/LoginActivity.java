9878127df464160354a0776ab33ee9e1a20f94ea
Anthony D <adas22@jhu.edu>
2020-04-07 01:47:57 -0400

    added fade animations

--- app/src/main/java/com/uima/cogs/LoginActivity.java
+++ app/src/main/java/com/uima/cogs/LoginActivity.java
@@ -40,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
         });
 
