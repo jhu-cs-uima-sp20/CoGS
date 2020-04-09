ff1de7d6febb5ee59ba4952212bcf547b65875b2
Madina Bah <bahmadina19@gmail.com>
2020-04-09 11:14:06 -0700

    Updated the create group page

--- app/src/main/java/com/uima/cogs/CreateGroupActivity.java
+++ app/src/main/java/com/uima/cogs/CreateGroupActivity.java
@@ -1,5 +1,6 @@
 package com.uima.cogs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,8 +12,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
@@ -46,20 +54,87 @@ public class CreateGroupActivity extends AppCompatActivity {
 
     void storeNewGroup(){
         String shortDescription = shortBio.getText().toString();
-        String courseName = groupCourse.getText().toString();
-        String name = groupName.getText().toString();
+        final String courseName = groupCourse.getText().toString();
+        final String courseId = courseName.replace(".", "");
+        final String name = groupName.getText().toString();
+        if(courseName.isEmpty() || courseId.isEmpty() || name.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Please Fill in All the Fields", Toast.LENGTH_SHORT).show();
 
-        GroupDescription newGroup = new GroupDescription();
-        newGroup.setShortDescription(shortDescription);
-        newGroup.setCourseName(courseName);
-        newGroup.setGroupName(name);
-        newGroup.addMembers(auth.getUid());
-        fireData.child("Groups").push().child("Group Description").setValue(newGroup).addOnSuccessListener(new OnSuccessListener<Void>() {
+        }
+        else {
+            GroupDescription newGroup = new GroupDescription();
+            newGroup.setShortDescription(shortDescription);
+            newGroup.setCourseName(courseName);
+            newGroup.setGroupName(name);
+            newGroup.addMembers(auth.getUid());
+            fireData.child("Groups").child(name).child("Group Description").setValue(newGroup).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(getApplicationContext(), "A New Group Has Been Created", Toast.LENGTH_SHORT).show();
+                    addGroupToCourse(courseId, name);
+                }
+            });
+        }
+
+    }
+
+    void addGroupToCourse(final String courseName, final String groupName){
+
+
+        DatabaseReference courseRef = fireData.child("Courses");
+        courseRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(getApplicationContext(), "A New Group Has Been Created", Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChild(courseName)){
+                    updateCourse(courseName, groupName);
+                }
+                else{
+
+                    createNewCourse(courseName, groupName);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
     }
+
+    public void createNewCourse(final String courseName, final String groupName){
+
+        DatabaseReference courseRef = fireData.child("Courses").child(courseName);
+        ArrayList<String> groupList = new ArrayList<>();
+        groupList.add(groupName);
+        courseRef.setValue(groupList);
+    }
+
+    public void updateCourse(final String courseName, final String groupName){
+
+        final DatabaseReference courseRef = fireData.child("Courses").child(courseName);
+
+        courseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                ArrayList<String> groupList = new ArrayList<>();
+                GenericTypeIndicator<ArrayList<String>> t = new GenericTypeIndicator<ArrayList<String>>() {};
+                groupList =  dataSnapshot.getValue(t);
+                groupList.add(groupName);
+                addGroupList(groupList, courseName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    void addGroupList(ArrayList<String> groupList, String courseName){
+        fireData.child("Courses").child(courseName).setValue(groupList);
+    }
+
 }
