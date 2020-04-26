2728986f2706525cd4a13192b9413a2b5bc68ccc
Anthony D <adas22@jhu.edu>
2020-04-26 00:56:21 -0400

    added tab layout and functionality to detailed group view

--- app/src/main/java/com/uima/cogs/GroupActivity.java
+++ app/src/main/java/com/uima/cogs/GroupActivity.java
@@ -1,48 +1,93 @@
 package com.uima.cogs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class GroupActivity extends AppCompatActivity {
 
     private Fragment meetingsFrag;
     private Fragment notesFrag;
-    private Button meetingsBtn;
     private FragmentTransaction transaction;
-    private Button groupSettingBtn;
     private String groupName;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
-        meetingsBtn = findViewById(R.id.meetingBtn);
         meetingsFrag = new MeetingsFragment();
+        toolbar = findViewById(R.id.group_toolbar);
+        tabLayout = findViewById(R.id.tabLayout);
 
         transaction = getSupportFragmentManager().beginTransaction();
-        meetingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                transaction.replace(R.id.group_fragment_container, meetingsFrag).commit();
-            }
-        });
 
         Intent intent = getIntent();
         groupName = intent.getStringExtra("Group Name");
 
-        groupSettingBtn = findViewById(R.id.groupSettings);
-        groupSettingBtn.setOnClickListener(new View.OnClickListener() {
+        toolbar.setTitle(groupName);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), GroupSettingsActivity.class);
-                intent.putExtra("Group Name", groupName);
-                startActivity(intent);
+                finish();
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        //transaction.replace(R.id.group_fragment_container, notesFrag).commit();
+                    case 1:
+                        transaction.replace(R.id.group_fragment_container, meetingsFrag).commit();
+                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
     }
-}
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main__group_detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settingsGroupIcon) {
+            Intent intent = new Intent(GroupActivity.this, GroupSettingsActivity.class);
+            intent.putExtra("Group Name", groupName);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.searchIcon) {
+            Intent intent = new Intent(GroupActivity.this, SearchGroupActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
