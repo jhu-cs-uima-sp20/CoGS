291525754bd7ce6e43aae171ed1ff967569eddce
andrewko <andrewko98@gmail.com>
2020-04-27 10:40:00 -0400

    created notes java class

--- app/src/main/java/com/uima/cogs/CreateNoteActivity.java
+++ app/src/main/java/com/uima/cogs/CreateNoteActivity.java
@@ -1,14 +1,67 @@
 package com.uima.cogs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private ImageView image;
+    private Button findNoteBtn;
+    private Button uploadNoteBtn;
+    private Intent intent;
+    private FirebaseAuth auth;
+    private SharedPreferences mPrefs;
+    private Toolbar toolbar;
+    private Uri filePath;
+    private String imageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        image = findViewById(R.id.noteImage);
+        findNoteBtn = findViewById(R.id.findNoteButton);
+        uploadNoteBtn = findViewById(R.id.uploadNoteButton);
+        toolbar = findViewById(R.id.notestoolbar);
+
+        auth = FirebaseAuth.getInstance();
+
+        uploadNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+    public void saveNote(String groupName, Notes note){
+        DatabaseReference noteRef = FirebaseDatabase.getInstance().getReference().child("Groups").child(groupName).child("Group Notes");
+        noteRef.push().setValue(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "New Note Has Been Successfully Created", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/uima/cogs/Notes.java
@@ -0,0 +1,26 @@
+package com.uima.cogs;
+
+import android.media.Image;
+
+public class Notes {
+    private String date;
+    private Image image;
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
+
+
